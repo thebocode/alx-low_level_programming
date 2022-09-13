6cc99d78b109a58076941da44582a7cd3a14ab7c
Bosun <bosunadebola@gmail.com>
2022-09-13 15:07:47 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ * Return: 1 for lowercase, 0 for uppercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
