b3250e20b60a531057c0750f6fe1cb52a6e3f6a8
Bosun <bosunadebola@gmail.com>
2022-09-14 10:29:02 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line
- * Return: always (0)
+ * Return: 0
  */
 
-int _putchar(char)
+int main(void)
 {
-	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(putchar); c++)
-	{
-		_putchar(putchar[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
