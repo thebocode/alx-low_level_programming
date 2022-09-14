b84820299c37a7d8523785fb7d342b43f0d6201e
Bosun <bosunadebola@gmail.com>
2022-09-13 18:18:00 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @c: first argument
+ * @d: second argument
+ * Return: the value of c + d
+ */
+
+int add(int c, int d)
+{
+	return (c + d);
+}
