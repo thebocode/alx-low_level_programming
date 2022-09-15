9eea02d82a9baedf07718174ec2de601777151b8
Bosun <bosunadebola@gmail.com>
2022-09-15 10:05:17 -0700

    3. Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int day, int month, int year);
+int convert_day(int day, int month);
 
 #endif /* MAIN_H */
