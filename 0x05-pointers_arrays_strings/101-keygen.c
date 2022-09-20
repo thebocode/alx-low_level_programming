ef3e29dd882380ceba138f699b8e511413ff1fe6
Bosun <bosunadebola@gmail.com>
2022-09-19 18:43:31 -0700

    11. Do not hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int r;
+
+	srand(time(0));
+
+	while (r <= 2645)
+	{
+		c = rand() % 128;
+		r += c;
+		putchar(c);
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
