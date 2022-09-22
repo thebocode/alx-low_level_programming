ba1426591d3abf54a8e0b4aa7ce212a07b67c7e1
Bosun <bosunadebola@gmail.com>
2022-09-22 12:40:45 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,29 +3,30 @@
 /**
  * cap_string - function that capitalizes all words of a string
  * @s: input string
- * Return: pointer to dest
+ * Return: address of the input string
  */
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"() {}";
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-	*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (*(s + i))
 	{
-		for (i = 0; i < 13; i++)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (*s + count == separators[i])
+			if (i == 0)
+			*(s + i) -= 'a' - 'A';
+			else
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-				*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
-		count++;
+		i++;
 	}
 	return (s);
 }
