74febf865fc8266462890b6f9c13e7b9c419f5e0
Bosun <bosunadebola@gmail.com>
2022-09-28 16:14:12 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,9 @@ include "main.h"
  * _pow_recursion - function that returns the natural square root of a number
  * @x: number to be raised
  * @y: power
+ *
  * Return: value of x raised to the power of y
+ * -1 if x < 0.
  */
 
 int _pow_recursion(int x, int y)
