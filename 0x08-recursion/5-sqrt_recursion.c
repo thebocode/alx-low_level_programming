5b63fbab7608762876441d73e00d2766b440fd1f
Bosun <bosunadebola@gmail.com>
2022-09-28 16:31:40 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,39 @@
 #include "main.h"
 
 /**
- * helperF
+ * helperFunction - to check if sqrt of number exists
+ * @num: number
+ * @pSqrt: possible sqrt
+ * Return: -1 if error, else sqrt of number
+ */
+
+int helperFunction(int num, int pSqrt)
+{
+	if ((pSqrt * pSqrt) == num)
+	{
+		return (pSqrt);
+	}
+
+	else
+	{
+		if ((pSqrt * pSqrt) > num)
+			return (-1);
+		else
+			return (helperFunction(num, pSqrt + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: number to find sqrt
+ * Return: sqrt of n
+ * -1 if error and does not have a natural sqrt
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (helperFunction(n, 0));
+}
