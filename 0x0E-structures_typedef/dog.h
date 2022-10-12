3294929a74c08dbaa10279a60d846d9493e451fd
Bosun <bosunadebola@gmail.com>
2022-10-12 04:40:35 -0700

    Completed tasks 0 - 2

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type to describe dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
