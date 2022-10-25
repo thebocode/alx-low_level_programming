49011b9f3f814c405cc13bbb257c1f6d7ad29b57
Bosun <bosunadebola@gmail.com>
2022-10-25 12:51:12 -0700

    13. Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,39 +13,39 @@ size_t free_listint_safe(listint_t **h);
 
 size_t looped_listint_count(listint_t *head)
 {
-	listint_t *tortoise, *hare;
+	listint_t *a, *b;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	a = head->next;
+	b = (head->next)->next;
 
-	while (hare)
+	while (b)
 	{
-		if (tortoise == hare)
+		if (a == b)
 		{
-			tortoise = head;
-			while (tortoise != hare)
+			a = b;
+			while (a != b)
 			{
 				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
+				a = a->next;
+				b = b->next;
 			}
 
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			a = a->next;
+			while (a != b)
 			{
 				nodes++;
-				tortoise = tortoise->next;
+				a = b->next;
 			}
 
 			return (nodes);
 		}
 
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		a = a->next;
+		b = (b->next)->next;
 	}
 
 	return (0);
