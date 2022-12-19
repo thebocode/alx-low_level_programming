1c2df0732f5e1b46921d1d81d114d2cd85a2e264
Bosun <bosunadebola@gmail.com>
2022-12-19 14:13:22 -0800

    updated tasks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		while (h->prev != NULL)
 			h = h->prev;
 	}
-	
+
 	new->next = h;
 
 	if (h != NULL)
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index
+ *  of a dlistint_t linked list
  * @head: head of the list
  * @index: index of the new node
  * Return: 1 if it success, -1 if it fails
